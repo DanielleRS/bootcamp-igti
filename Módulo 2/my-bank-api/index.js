fa2df3e4d9ec0bb6a47e42734141f13fb97959dd
Danielle <daniellerodris@gmail.com>
2020-06-15 15:39:59 -0300

    Desafio mod 1, trabalho pratico e aula mod 2, criação mod 3

---
+++ Módulo 2/my-bank-api/index.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var fs = require("fs");
+
+var app = express();
+
+app.use(express.json());
+
+app.post("/account", (req, res) => {
+    let account = req.body;
+
+    fs.readFile("accounts.json", "utf8", (err, data) => {
+        if(!err) {
+            try {
+                let json = JSON.parse(data);
+                account = { id: json.nextId++, ...account };
+                json.accounts.push(account);
+
+                fs.writeFile("accounts.json", JSON.stringify(json), err => {
+                    if(err){
+                        res.status(400).send({ error: err.message });
+                    } else {
+                        res.end();
+                    }
+                });
+            } catch(err) {
+                res.status(400).send({ error: err.message });
+            }
+        } else {
+            res.status(400).send({ error: err.message });
+        }
+    });
+});
+
+app.listen(3000, function () {
+    try {
+        fs.readFile("accounts.json", "utf8", (err, data) => {
+            if(err) {
+                const initialJson = {
+                    nextId: 1,
+                    accounts: []
+                };
+                fs.writeFile("accounts.json", JSON.stringify(initialJson), err => {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            }
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
